34ed51bddd758f08cfb577eb9889c319214585f8
Matheus <matheuus412@gmail.com>
2022-05-16 00:02:59 -0300

    Upload to Vercel2

--- web/src/components/WidgetForm/index.tsx
+++ web/src/components/WidgetForm/index.tsx
@@ -1,7 +1,7 @@
 import { CloseButton } from "../CloseButton";
-import bugImageUrl from "../../assets/bug.svg";
-import ideaImageUrl from "../../assets/idea.svg";
-import thoughtImageUrl from "../../assets/thought.svg";
+import bugImageUrl from "../../assets/Bug.svg";
+import ideaImageUrl from "../../assets/Idea.svg";
+import thoughtImageUrl from "../../assets/Thought.svg";
 import { useState } from "react";
 import { FeedbackTypesStep } from "./Steps/FeedbackTypesStep";
 import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
